e8da9193acd777f98bf3f64ed2495fe866bf2d6a
gideonsarpong <nyamekyegideonsarpong@gmail.com>
2025-12-07 17:08:19 +0000

    Updated to add Sonar test coverage

---
+++ ckd-decision-support/Node/jest.config.js
@@ -0,0 +1,18 @@
+/** @type {import('jest').Config} */
+const config = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "**/*.{js,jsx,ts,tsx}",
+    "!**/node_modules/**",
+    "!**/.next/**",
+    "!**/public/**",
+  ],
+  coverageDirectory: "coverage",
+};
+
+module.exports = config;
